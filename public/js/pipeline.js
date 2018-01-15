bf39a4c707522411e6ec4df9c74d1a2c8ac1de68
Jeff Sincavage <jsincav44@gmail.com>
2018-01-14 19:22:27 -0500

    installed socket.io

--- public/js/pipeline.js
+++ public/js/pipeline.js
@@ -1,13 +1,60 @@
-//var socket = io();
+var socket = io();
 var ctx = $('#numberChart');
 
+var numberData = {
+    labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7"],
+    datasets: [{
+        label: 'Pre-Qualified Pipeline',
+        backgroundColor: window.chartColors.red,
+        borderColor: window.chartColors.red,
+        data: [
+            100,
+            150,
+            140,
+            122,
+            145,
+            166,
+            184
+        ],
+        fill: false,
+    }, {
+        label: 'Qualified Pipeline',
+        fill: false,
+        backgroundColor: window.chartColors.blue,
+        borderColor: window.chartColors.blue,
+        data: [
+            44,
+            87,
+            12,
+            88,
+            90,
+            122,
+            100
+        ],
+    }, {
+        label: 'Total Pipeline',
+        fill: false,
+        backgroundColor: window.chartColors.grey,
+        borderColor: window.chartColors.grey,
+        data: [
+            200,
+            220,
+            240,
+            202,
+            232,
+            200,
+            250
+        ],
+    }]
+}
+
 $(function() {
-    testGenerate();
+    generateCharts();
 });
 
-function generageCharts(numberData,ageData) {
+function generateCharts() {
 
-    var numberChart = new Chart(numberctx, {
+    var numberChart = new Chart(ctx, {
         type: 'line',
         data: numberData,
         options: {
@@ -21,9 +68,9 @@ function generageCharts(numberData,ageData) {
 
     ctx = $('#ageChart');
 
-    var ageChart = new Chart(agectx, {
+    var ageChart = new Chart(ctx, {
         type: 'line',
-        data: ageData,
+        data: numberData,
         options: {
             elements: {
                 line: {
@@ -32,67 +79,4 @@ function generageCharts(numberData,ageData) {
             }
         }
     });
-}
-
-function testGenerate() {
-
-    ctx = $('#ageChart');
-
-    var ageChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7"],
-            datasets: [{
-                label: 'Pre-Qualified Pipeline',
-                backgroundColor: window.chartColors.red,
-                borderColor: window.chartColors.red,
-                data: [
-                    100,
-                    150,
-                    140,
-                    122,
-                    145,
-                    166,
-                    184
-                ],
-                fill: false,
-            }, {
-                label: 'Qualified Pipeline',
-                fill: false,
-                backgroundColor: window.chartColors.blue,
-                borderColor: window.chartColors.blue,
-                data: [
-                    44,
-                    87,
-                    12,
-                    88,
-                    90,
-                    122,
-                    100
-                ],
-            }, {
-                label: 'Total Pipeline',
-                fill: false,
-                backgroundColor: window.chartColors.grey,
-                borderColor: window.chartColors.grey,
-                data: [
-                    200,
-                    220,
-                    240,
-                    202,
-                    232,
-                    200,
-                    250
-                ],
-            }]
-        },
-        options: {
-            elements: {
-                line: {
-                    tension: 0, // disables bezier curves
-                }
-            }
-        }
-});
-
 }
--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
 var {client} = require('./db');
 
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
-
-const publicPath = path.join(__dirname, '../public');
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
@@ -14,11 +17,30 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve('public/pipeline.html'));
 });
 
-client.query('SELECT * FROM num', (err, res) => {
-    console.log(err, res)
-    client.end()
+//queries
+io.on('connection', (socket) => { 
+    console.log('New user connected');
+
+    // socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+
+    // socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+
+    // socket.on('createMessage', (message, callback) => {
+    //     console.log('createMessage', message);
+    //     io.emit('newMessage', generateMessage(message.from,message.text));
+    //     callback('This is from the server');
+    // });
+
+    // socket.on('disconnect', () => {
+    //     console.log('User was disconnected');
+    // });
 });
 
-app.listen(port,() => {
+// client.query('SELECT * FROM num', (err, res) => {
+//     console.log(err, res)
+//     client.end()
+// });
+
+server.listen(port,() => {
     console.log(`Server is up on port ${port}`);
 });
