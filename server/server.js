bb3ed2d382d386fba503087cc4b050c2d650538e
Jeff Sincavage <jsincav44@gmail.com>
2018-01-16 20:29:15 -0500

    added handlebars and moved some files around

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const hbs = require('hbs');
 const path = require('path');
 const http = require('http');
 const socketIO = require('socket.io');
@@ -11,10 +12,21 @@ var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
+hbs.registerPartials(path.join(__dirname, '../views/partials'));
+console.log(path.join(__dirname, '../views'));
+app.set('view engine', 'hbs');
 app.use(express.static(publicPath));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve('public/pipeline.html'));
+    res.render('index.hbs', {
+        pageTitle: 'Lingraphica Dashboards'
+    });
+});
+
+app.get('/pipeline', (req, res) => {
+    res.render('pipeline.hbs', {
+        pageTitle: 'Pipeline Snaphot'
+    });
 });
 
 var query = client.query("LISTEN watchers");
